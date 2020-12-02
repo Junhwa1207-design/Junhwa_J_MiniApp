eec639904dc5cd21913c5fb3eb4de8c555e78083
Juna <71682432+Junhwa1207-design@users.noreply.github.com>
2020-12-02 04:48:21 -0500

    add includes

---
+++ includes/functions.php
@@ -0,0 +1,34 @@
+<?php
+    //$result will store the database request results so that we can encode and return them to index.php
+$result = array();
+
+function getAllUsers($conn)
+{
+    $query = "SELECT * FROM miniCooper";
+
+    $runQuery = $conn->query($query);
+
+    while ($row = $runQuery->fetchAll(PDO::FETCH_ASSOC)) {
+        $result[] = $row;
+    }
+
+    //return $result;
+    echo(json_encode($result));
+}
+
+//get a specific user
+function getSingleUser($conn, $id)
+{
+    $query = "SELECT * FROM miniCooper WHERE id=" . $id . "";
+
+    
+
+    $runQuery = $conn->query($query);
+
+    while ($row = $runQuery->fetchAll(PDO::FETCH_ASSOC)) {
+        $result[] = $row;
+    }
+
+    //return $result;
+    echo(json_encode($result));
+}
